edb2b3422a1058f04d7f28b9c22f21137fe96e82
xiaoye <zxiaoye201@gmail.com>
2022-03-10 15:34:15 +0800

    second commit

--- src/main/java/com/renwen/git/GitTest.java
+++ src/main/java/com/renwen/git/GitTest.java
@@ -6,5 +6,6 @@ package com.renwen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
